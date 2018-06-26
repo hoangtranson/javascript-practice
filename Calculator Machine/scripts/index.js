bcac965862763e184da5dd0592fb527c43d86ee5
Tran Son Hoang <hoang.transon@pycogroup.com>
2018-06-26 23:12:19 +0700

    Implement calculator module and add cypress to end to end test

--- Calculator Machine/scripts/index.js
+++ Calculator Machine/scripts/index.js
@@ -3,18 +3,40 @@
  * - All our useful JS goes here, awesome!
  */
 /* limpa o display */ 
-document.getElementById("clear").addEventListener("click", function() {
-	document.getElementById("display").value = "";
-});
-/* recebe os valores */
-function get(value) {
-	document.getElementById("display").value += value; 
-} 
+// document.getElementById("clear").addEventListener("click", function() {
+// 	document.getElementById("display").value = "";
+// });
 
-/* calcula */
-function calculates() {
-	var result = 0;
-	result = document.getElementById("display").value;
-	document.getElementById("display").value = "";
-	document.getElementById("display").value = eval(result);
-};
+
+const calculatorModule = (function(){
+	const clear_btn = getElementById('clear');
+	const display_screen = getElementById('display');
+	const button = getElementByClass('calculator__btn');
+
+	let { value } = display_screen;
+
+	clear_btn.addEventListener('click', () => {
+		value = '';
+	});
+
+	for (var i = 0; i < button.length; i++) {
+		button[i].addEventListener('click', (event) => {
+			value += event.target.value; 
+			display_screen.value = calculates();
+		});
+	}
+
+	/* calcula */
+	function calculates() {
+		const temp = value;
+		return eval(temp);
+	};
+
+	function getElementById(Id){
+		return document.getElementById(Id);
+	}
+
+	function getElementByClass(cl){
+		return document.getElementsByClassName(cl);
+	}
+})();
